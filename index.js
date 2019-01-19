9ce374166086da69a989ba408c996d2a6a885251
Daniel Shimield <dxshim90@gmail.com>
2019-01-19 17:13:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function dwarfRollCall(dwarves) {
-}
+function dwarfRollCall(dwarves){
+   var array =[]
+   for (var i=0;i<dwarves.length;i++){
+     array.push(`${i+1}. ${dwarves[i]} `)
+   }
+   return array.join("")
+ }
 
 function summonCaptainPlanet(planeteerCalls){
+var newarr = planeteerCalls.map(x => x.toUpperCase() + "!");
+return newarr;
 }
 
 function longPlaneteerCalls(words) {
+for (var i = 0; i < words.length; i++){
+  if (words[i].length > 4) {
+  return true
+  }
+}
+return false
 }
 
+
 function findTheCheese (foods) {
-}
+  for (var i = 0; i < foods.length; i++) {
+  if ((foods[i] ==="cheddar") || (foods[i] === "gouda") || (foods[i] ==="camembert")){
+    return foods[i];
+  }
+  }
+  return "no cheese!"
+  }
+
